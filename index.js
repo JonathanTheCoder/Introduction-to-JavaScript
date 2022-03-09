5cf451820cc587b6e1d52253872867412d0889d5
JonathanTheCoder <spectrumrecords21@gmail.com>
2022-03-09 18:41:48 -0500

    Completed Tasks

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ Do the following:
    HINT: no function required
 */
 let dog = 'bob';
-let behaviour = 'bad'
-if(behaviour = 'good') {
+let behavior = 'bad'
+if(behavior = 'good') {
   dog = 'bob'; 
-} else if(behaviour === 'bad'){
+} else if(behavior === 'bad'){
   dog = 'steve';
 }
 console.log(dog);
@@ -55,7 +55,8 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+const party = '1999'
+console.log('task 1 c:', Number(party));
 
 
 
@@ -136,10 +137,26 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1 && weight <=5){
+    return weight * 0.05;
+  }else if(age >= 1 && weight >=6 && weight <=10) {
+    return weight * 0.04;
+  }else if(age >= 1 && weight >=11 && weight <=15){
+    return weight * 0.03
+  }else if(age >= 1 && weight > 15){
+    return weight *0.02 
+  }else if (age < 1 && age >= 0.583){
+    return weight *0.04
+  }else if (age < 0.583 && age >= 0.333){
+    return weight *0.05
+  }else if (age < 0.333){
+    return weight * 0.10;
+  }else{
+    return 'please try again'
+  }   
 }
-
+console.log('task 3')
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -162,12 +179,31 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
+let computer = Math.random();
 
-function game(user, computer){
-  /*add your code here*/
+if(computer <= 0.34) {
+  computer = 'rock'
+}else if(computer <= 0.67){
+  computer = 'paper'
+}else if (computer > 0.67){
+  computer ='scissors'
 }
 
+function game(user, computer){
+  if(user === computer){
+    return `its a tie`
+  }else if(user === 'rock' && computer == 'scissors'){
+    return `you win`;
+  }else if(user === 'paper' && computer === 'rock'){
+    return `you win`;
+  }else if(user === 'scissors' && computer === 'paper'){
+    return `you win`;
+  }else{
+    return `you lose`
+  }
+}
 
+console.log('task 4', game('paper', computer))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -180,11 +216,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log(miles(20));
 
 //Task 5b - Feet to CM
 /*
@@ -194,11 +230,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48
 }
 
-
+console.log(feet(12))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -214,10 +250,13 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for(let i = number; 1 > 0; i--){
+        return `${i} bottles of soda on the wall, ${1} bottles of soda, take one down pass it around ${i -1} bottles of soda on the wall`;
+      }
 }
-
+console.log(annoyingSong(7))
+console.log(annoyingSong(2))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -234,11 +273,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+if(number >= 90){
+  return `you got an A`;
+}else if(number < 90 && number >= 80){
+  return `you got a B`
+}else if( number < 80 && number >= 70){
+  return `you got a C`
+}else if(number < 70 && number >= 60){
+  return `you got a D`
+}else if(number > 60){
+  return `you got an F`
+}
 }
 
-
+console.log(grade(85))
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
